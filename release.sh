29953a230ad74ead6eb2ff91e5aad6600b953a77
Maarten Wullink <maarten.wullink@sidn.nl>
2025-05-13 09:28:11 +0200

    update jave version

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=`/usr/libexec/java_home -v 17`
+export JAVA_HOME=`/usr/libexec/java_home -v 21`
 export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
 
 mvn release:clean && \
